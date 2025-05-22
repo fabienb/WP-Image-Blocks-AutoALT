3e53a359aae3fe54e867d664e5b038f4a927a3f7
Fabien Butazzi <fabien.b@gmail.com>
2025-05-22 14:39:20 +0100

    Moved away from str_replace to use regular expressions and improve performance

--- automatic-alt-tags-on-img.php
+++ automatic-alt-tags-on-img.php
@@ -23,41 +23,39 @@ add_filter('render_block', 'tct_add_alt_tags', 10, 2);
  */
 function tct_add_alt_tags(string $content, array $block): string
 {
-	// Get the block name
-	$block_name = $block['blockName'];
+		// Get the block name
+		$block_name = $block['blockName'];
 
-	// Define target blocks that need ALT tags to be added
-	$target_blocks = [
-		'core/image',
-		'generateblocks/image',
-		// EditorsKit
-		'editorskit/image',
-		'editorskit/advanced-image',
-		// QI Blocks
-		'qi-blocks/media-image',
-		'qi-blocks/image-slider',
-		'qi-blocks/image-gallery', // Add more if needed
-	];
+		// Define target blocks that need ALT tags to be added
+		$target_blocks = [
+				'core/image',
+				'generateblocks/image',
+				'editorskit/image',
+				'editorskit/advanced-image',
+				'qi-blocks/media-image',
+				'qi-blocks/image-slider',
+				'qi-blocks/image-gallery',
+		];
 
-	// Check if the current block is one of the target blocks
-	if (!in_array($block_name, $target_blocks)) {
-		return $content;
-	}
-
-	// Determine the image ID(s) based on the block type
-	$image_ids = get_image_ids_from_block($block);
-
-	// Iterate over each image ID and add ALT tags
-	foreach ($image_ids as $id) {
-		if ($id !== 0) {
-			$alt = get_post_meta($id, '_wp_attachment_image_alt', true);
-			if (!empty($alt)) {
-				$content = update_image_with_alt_tag($content, $id, $alt);
-			}
+		// Check if the current block is one of the target blocks
+		if (!in_array($block_name, $target_blocks)) {
+				return $content;
 		}
-	}
 
-	return $content;
+		// Determine the image ID(s) based on the block type
+		$image_ids = get_image_ids_from_block($block);
+
+		// Iterate over each image ID and add ALT tags
+		foreach ($image_ids as $id) {
+				if ($id !== 0) {
+						$alt = get_post_meta($id, '_wp_attachment_image_alt', true);
+						if (!empty($alt)) {
+								$content = update_image_with_alt_tag($content, $id, $alt);
+						}
+				}
+		}
+
+		return $content;
 }
 
 /**
@@ -68,22 +66,22 @@ function tct_add_alt_tags(string $content, array $block): string
  */
 function get_image_ids_from_block(array $block): array
 {
-	$block_name = $block['blockName'];
-	$image_ids = [];
+		$block_name = $block['blockName'];
+		$image_ids = [];
 
-	return match ($block_name) {
-		'generateblocks/image' => [$block['attrs']['mediaId'] ?? 0],
-		'editorskit/image', 'editorskit/advanced-image' => [$block['attrs']['imageID'] ?? 0],
-		'qi-blocks/media-image' => [$block['attrs']['mediaId'] ?? 0],
-		'qi-blocks/image-slider', 'qi-blocks/image-gallery' => array_map(function ($image) {
-			return $image['id'] ?? 0;
-		}, $block['attrs']['images'] ?? []),
-		default => [$block['attrs']['id'] ?? 0],
-	};
+		return match ($block_name) {
+				'generateblocks/image' => [$block['attrs']['mediaId'] ?? 0],
+				'editorskit/image', 'editorskit/advanced-image' => [$block['attrs']['imageID'] ?? 0],
+				'qi-blocks/media-image' => [$block['attrs']['mediaId'] ?? 0],
+				'qi-blocks/image-slider', 'qi-blocks/image-gallery' => array_map(function ($image) {
+						return $image['id'] ?? 0;
+				}, $block['attrs']['images'] ?? []),
+				default => [$block['attrs']['id'] ?? 0],
+		};
 }
 
 /**
- * Update the image tag with the ALT attribute.
+ * Update the image tag with the ALT attribute using regular expressions.
  *
  * @param string $content The block content.
  * @param int $id The image ID.
@@ -92,24 +90,29 @@ function get_image_ids_from_block(array $block): array
  */
 function update_image_with_alt_tag(string $content, int $id, string $alt): string
 {
-	// Escape the alt text to ensure it's safe for output
-	$escaped_alt = esc_attr($alt);
+		// Escape the alt text to ensure it's safe for output
+		$escaped_alt = esc_attr($alt);
 
-	// Get the image URL from the ID
-	$image_url = wp_get_attachment_url($id);
+		// Get the image URL from the ID
+		$image_url = wp_get_attachment_url($id);
 
-	if (!$image_url) {
-		return $content;
-	}
-
-	// Check if the image tag already has an empty alt attribute and replace it
-	if (false !== strpos($content, 'src="' . $image_url . '"')) {
-		if (false !== strpos($content, 'alt=""')) {
-			$content = str_replace('alt=""', 'alt="' . $escaped_alt . '"', $content);
-		} elseif (false === strpos($content, 'alt="')) {
-			$content = str_replace('src="' . $image_url . '"', 'alt="' . $escaped_alt . '" src="' . $image_url . '"', $content);
+		if (!$image_url) {
+				return $content;
 		}
-	}
 
-	return $content;
+		// Create a regular expression pattern to match the image tag with the given src attribute
+		$pattern = '/<img([^>]*?)src="' . preg_quote($image_url, '/') . '"([^>]*?)(?:alt="([^"]*)")?([^>]*?)\/?>/i';
+
+		// Replace the matched image tags with updated ALT attributes
+		return preg_replace_callback($pattern, function ($matches) use ($escaped_alt) {
+				if (!empty($matches[3])) {
+						// If an existing alt attribute is found, replace it with the new value
+						$replacement = '<img' . $matches[1] . 'src="' . $matches[2] . '"alt="' . $escaped_alt . '"' . $matches[4] . ' />';
+				} else {
+						// If no alt attribute is found, add the new value
+						$replacement = '<img' . $matches[1] . 'src="' . $matches[2] . '"alt="' . $escaped_alt . '"' . $matches[4] . ' />';
+				}
+
+				return $replacement;
+		}, $content);
 }
